3f6a71deabd20135d34aa1cf594bb95e792556cf
DanielKatt <89039660+DanielKatt@users.noreply.github.com>
2021-12-14 10:43:02 -0500

    Transitions.

--- fondo.js
+++ fondo.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.container')
-const colors = ['#CB51EE', '#0073BC', '#38B753E', '#DE356C']
+const colors = ['#CB51EE', '#0073BC', '#38B753E', '#DE365C']
 
 const figures = () => {
-    for(let i = 0; i <= 25; i++){
+    for(let i = 0; i <= 50; i++){
         let figure = document.createElement('span')
         let select = Math.round(colors.length * Math.random())
         
@@ -13,6 +13,12 @@ const figures = () => {
         figure.style.background = colors[select >= colors.length ? select - 1: select]
  
         container.appendChild(figure)
+
+        setInterval(() => {
+            figure.style.top = innerHeight * Math.random() + 'px'
+            figure.style.left = innerWidth * Math.random() + 'px'
+        }, 3000)
+
     }
 }
 
